ab2a0ede25a2f6352e1ba7e54ddb5c7101d43526
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-11-03 18:43:48 +0000

    ADT accao implemented. Closes #3 #4 #5.

--- src/tetris.lisp
+++ src/tetris.lisp
@@ -1,6 +1,18 @@
 ;;;; Grupo 49: Illya Gerasymchuk (78134), Nuno Silva (78454), Jorge Heleno (79042) ;;;;
 ;;;; Tetris source file
 
+;;; Acao [2.2.1]
+(defun cria-accao(leftmost-col piece)
+    (cons leftmost-col piece))
+
+(defun accao-coluna(action) 
+    (car action))
+
+(defun accao-peca(action) 
+    (cdr action))
+
+
+;;; Tabuleiro [2.1.2]
 (defun cria-tabuleiro (&optional (row 18) (col 19))
     (make-array (list row col)))
 
