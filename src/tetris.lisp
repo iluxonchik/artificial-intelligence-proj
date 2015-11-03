5bc6e5b92e80c7f2459ec04792703c156840309f
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-11-03 18:10:26 +0000

    Fix for function tabuleiro.
    * Syntax fixes

--- src/tetris.lisp
+++ src/tetris.lisp
@@ -1,8 +1,6 @@
 ;;;; Grupo 49: Illya Gerasymchuk (78134), Nuno Silva (78454), Jorge Heleno (79042) ;;;;
 ;;;; Tetris source file
 
-(defun cria-tabuleiro(&optional row &optional col) 
-  (if (null row) (setf row 18)) 
-  (if (null col) (setf col 19)) 
-  (make-array (cons row (list col))))
+(defun cria-tabuleiro (&optional (row 18) (col 19))
+    (make-array (list row col)))
 
