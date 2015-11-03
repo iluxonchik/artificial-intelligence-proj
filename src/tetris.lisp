41fb241af45e8f8f3f3997a40c84e69a170d3e9b
Gisson <jorgehelenoist@gmail.com>
2015-11-03 01:28:45 +0000

    Added a suggestion to cria-tabuleiro function. Where it creates a list of list(making it a matrix) of Row * Column. Regarding #1

--- src/tetris.lisp
+++ src/tetris.lisp
@@ -1,3 +1,9 @@
 ;;;; Grupo 49: Illya Gerasymchuk (78134), Nuno Silva (78454), Jorge Heleno (79042) ;;;;
 ;;;; Tetris source file
 
+(defun try-tab(row col) (cond 
+                         ( (zerop row) '()) 
+                         (t (setf aux '()) (dotimes (n col)
+                                             (setf aux (append aux (list 0) )))
+                            (cons aux (try-tab (1- row) col)))))
+
