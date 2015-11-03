5a91e53c315ab7a24f9fc8d649571ba925e87f46
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-11-03 19:42:33 +0000

    Estado ADT implemented [Section 2.1.3]
    Closes #22, closes #16, closes #17, closes #18.

--- src/tetris.lisp
+++ src/tetris.lisp
@@ -2,13 +2,13 @@
 ;;;; Tetris source file
 
 ;;; Acao [2.2.1]
-(defun cria-accao(leftmost-col piece)
+(defun cria-accao (leftmost-col piece)
     (cons leftmost-col piece))
 
-(defun accao-coluna(action) 
+(defun accao-coluna (action) 
     (car action))
 
-(defun accao-peca(action) 
+(defun accao-peca (action) 
     (cdr action))
 
 
@@ -16,3 +16,19 @@
 (defun cria-tabuleiro (&optional (row 18) (col 19))
     (make-array (list row col)))
 
+
+;;; Estado [2.1.3]
+(defstruct estado
+    pontos
+    pecas-por-colocar   ; list ordered by exit order  | legal values: i,j,l,o,s,z,t
+    pecas-por-colocadas ; list ordered by most recent | legal values: i,j,l,o,s,z,t
+    Tabuleiro)
+
+(defun copia-estado (state)
+    (copy-structure state))
+
+(defun estados-iguais-p (state1 state2)
+    (equalp state1 state2))
+
+(defun estado-final-p (state)
+    (or (tabuleiro-topo-preenchido-p (estado-Tabuleiro state)) (null (estado-pecas-por-colocar state))))
